f5a787eaea31e9da46fb6dde686176376215ff8b
Seungkook lee <lskook47@gmail.com>
2025-03-27 20:35:57 +0900

    revise resetting sh

--- scripts/reset_cluster.sh
+++ scripts/reset_cluster.sh
@@ -7,8 +7,12 @@ sudo ufw disable
 
 # delete existing flannel cni
 
-sudo ip link delete flannel.1 
-sudo ip link delete cni0 
+sudo ip link set flannel.1 down
+sudo ip link del flannel.1
+
+sudo ip link set cni0 down
+sudo ip link del cni0
+
 sudo rm $HOME/.kube/config
 sudo modprobe br_netfilter
 sudo sysctl net.bridge.bridge-nf-call-iptables=1
